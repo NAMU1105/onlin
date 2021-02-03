735cb79b7d3a555ce067754e77b5826c6c420427
JongHakSeo <unqocn@gmail.com>
2021-02-03 22:16:59 +0900

    fix: hide scroll bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,16 @@ const IndexPage = () => (
     <Global
       styles={css`
         ${emotionReset}
-        *, *::after, *::before {
+        ::-webkit-scrollbar {
+          display: none; /* Chrome, Safari, Opera*/
+        }
+        *,
+        *::after,
+        *::before {
           font-family: "Nanum Myeongjo", "Courier New";
+          -ms-overflow-style: none; /* IE and Edge */
+          scrollbar-width: none; /* Firefox */
+
           box-sizing: border-box;
           line-height: 1.5;
           -moz-osx-font-smoothing: grayscale;
